84d847fdf51390280f78bc8caeaa2af5ae331e0e
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-17 00:00:23 +0530

    fizz buzz

---
+++ src/main/java/dsa/FizzBuzz.java
@@ -0,0 +1,32 @@
+package dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+	
+	public static List<String> fizzBuzz(int n) {
+		List<String> fizzBuzzList = new ArrayList<>();
+		String answers[] = { "FizzBuzz", "Buzz", "Fizz", "0" };
+		for (int i = 1; i <= n; i++) {
+			int index = 3;
+			answers[3] = String.valueOf(i);
+			if (i % 3 == 0)
+				index--;
+			if (i % 5 == 0)
+				index -= 2;
+			fizzBuzzList.add(answers[index]);
+
+		}
+		// System.out.println(fizzBuzzList);
+
+		return fizzBuzzList;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fizzBuzz(21));
+
+	}
+
+}
