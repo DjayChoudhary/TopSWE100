9ec0efac405f654d900a0f9fcb0101afe8f354b1
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-17 00:30:42 +0530

    add Two Sum

---
+++ src/main/java/dsa/TwoSum.java
@@ -0,0 +1,20 @@
+package dsa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+
+		Map<Integer, Integer> hashMap = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (hashMap.containsKey(target - nums[i])) {
+				return new int[] { i, hashMap.get(target - nums[i]) };
+			}
+			hashMap.put(nums[i], i);
+		}
+
+		return null;
+	}
+}
